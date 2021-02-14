72a9b446b59f287e863b6f2c625ce52ebbc56d1b
rupenman <rupenman>
2021-02-13 19:14:15 -0500

    Product Microservice v0.0

---
+++ backend/product/src/main/java/org/ecom/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.ecom.product.repository;
+
+import org.ecom.product.domain.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+}
--- backend/product/src/main/java/org/ecom/product/rest/ProductRestResource.java
+++ backend/product/src/main/java/org/ecom/product/rest/ProductRestResource.java
@@ -1,30 +1,40 @@
 package org.ecom.product.rest;
 
 import org.ecom.product.domain.Product;
+import org.ecom.product.service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ProductRestResource {
 
+    private ProductService productService;
+
+    public ProductRestResource(ProductService productService) {
+        this.productService = productService;
+    }
     @GetMapping("/products")
-    public List<Product> fetchProducts() {
-        throw new UnsupportedOperationException();
+    public Iterable<Product> fetchProducts() {
+       return productService.fetchProducts();
     }
 
     @GetMapping(value = "/products/{id}")
     public Product fetchProductById(@PathVariable Long id) {
-        throw new UnsupportedOperationException();
+        return productService.fetchProductById(id);
     }
 
     @PostMapping("/products")
-    public Product ***REMOVED***veProduct(@RequestBody Product product) {
-        throw new UnsupportedOperationException();
+    public Product saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
     }
 
     @DeleteMapping("/products/{id}")
     public void deleteProductById(@PathVariable Long id) {
-        throw new UnsupportedOperationException();
+        productService.deleteProductById(id);
+    }
+
+    @GetMapping("/products")
+    public Page<Product> fetchProductsById(@RequestParam  int page, @RequestParam int size, @RequestParam String direction, @RequestParam String sortBy) {
+        return productService.fetchProducts(page, size, direction, sortBy);
     }
 }
